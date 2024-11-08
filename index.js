664d3651ff752468b4277a5ee72ccd31dbecffb1
Md. Parvez Mahmud <freelancherparvez@gmail.com>
2024-11-08 13:57:06 +0600

    product area

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
-var parvez = "This is Bangladesh"
-var car = parvez.charAt(2);
-console.log(car);
+// // var parvez = "This is Bangladesh"
+// // var car = parvez.charAt(2);
+// // console.log(car);
+// // const students = [
+// //     {
+// //       name: "Alice",
+// //       age: 22,
+// //       major: "Computer Science"
+// //     },
+// //     {
+// //       name: "Bob",
+// //       age: 25,
+// //       major: "Mathematics"
+// //     },
+// //     {
+// //       name: "Charlie",
+// //       age: 23,
+// //       major: "Physics"
+// //     }
+// //   ];
+// //   for( var sub of students){
+// //     console.log(sub.major)
+// //   }
+// function name() {
+//     const a = 2;
+//     const B = 12;
+//     const c = 13;
+//     const d = 14;
+//     const e = 15;
+//     const add = a+B+c+d+e
+//     return add
+
+const { checkEvenOdd, num } = require("./even");
+
+    
+// }
+// const result = name()
+// console.log("Addition Result =",result)
+// //second function
+// function multiname() {
+//     const a = 2;
+//     const B = 12;
+//     const c = 13;
+//     const d = 14;
+//     const e = 15;
+//     const multi = a*B*c*d*e
+//     return multi
+
+    
+// }
+// const multiresult = multiname()
+// console.log("Multiapplication Result =",multiresult)
+// //third fuction
+// function subname() {
+//     const a = 2;
+//     const B = 12;
+//     const c = 13;
+//     const d = 14;
+//     const e = 15;
+//     const multi = a-B-c-d-e
+//     return multi
+ 
+// }
+// const sub = subname()
+// console.log( "Subtraction Result =",sub)
+// //paramitter pass  
+// function pass(p,q) {
+//     return p*q
+// }
+// console.log(pass(17,18));
+console.log(checkEvenOdd(num));
+
